ee6e11c491b83a280c97a3de48ec2f6b474670bd
Alex Brady <alex.brady@blueface.com>
2021-03-26 16:59:12 +0000

    track high/low occurance drivers

--- scheduler/process.py
+++ scheduler/process.py
@@ -9,7 +9,7 @@ from collections import defaultdict
 from scheduler.validate import Schemas
 
 
-def process() -> typing.DefaultDict[str, typing.Any]:
+def process() -> typing.Dict[str, typing.Dict[str, typing.Dict[int, int]]]:
     try:
         schemas = Schemas()
     except ValueError:
@@ -33,7 +33,7 @@ def process() -> typing.DefaultDict[str, typing.Any]:
             if shift == 1 or shift == 2:
                 driver_shifts[day.name].append((i + 1, shift))
 
-    result = defaultdict(list)
+    result = {}
     drivers_per_route = {}
     driver_night_shifts = {driver: 0 for driver in schemas.qualified_routes.index}
 
@@ -50,40 +50,69 @@ def process() -> typing.DefaultDict[str, typing.Any]:
 
             route_result[route_name] = possible_route_drivers
 
-            drivers_per_route[day]  = route_result
+            drivers_per_route[day] = route_result
         result[day] = drivers_per_route[day]
 
-        drivers_per_route[day] = dict(sorted(route_result.items(), key=lambda i: len(i[1]), reverse=False))
+        drivers_per_route[day] = dict(sorted(route_result.items(), key=lambda r: len(r[1]), reverse=False))
+
+    driver_counts = {driver: 0 for driver in schemas.qualified_routes.index}
+    for day, routes in drivers_per_route.items():
+        for r in qualified_drivers.keys():
+            for driver in routes[r]:
+                driver_counts[driver] += 1
+
+    min_value = max(driver_counts.values())
+    high_occurrence = [k for k in driver_counts if driver_counts[k] == min_value]
+    low_occurrence = [k for k in driver_counts.keys() if k not in high_occurrence]
 
     for day, routes in drivers_per_route.items():
+        print(f'Day: {day}')
         route_result = {}
         previous_route_drivers = []
         leftover_drivers_per_route = {}
 
         for route_name, route_drivers in routes.items():
+            print(f'Route: {route_name}')
             possible_route_drivers = []
 
             # Keep track of riders who have already worked today.
             for _, vals in route_result.items():
                 previous_route_drivers.extend(list(vals.values()))
 
-            # Check that driver is qualified to drive route, and has not booked the day off.
+            print(f'Previous drivers: {previous_route_drivers}')
+
+            # Check that the driver has not already worked today.
             for driver in route_drivers:
                 if driver not in previous_route_drivers:
                     possible_route_drivers.append(driver)
+
+            print(f'Potential drivers: {possible_route_drivers}')
 
             night_drivers = {}
             for driver in possible_route_drivers:
                 if driver_night_shifts[driver] < 4:
                     night_drivers[driver] = driver_night_shifts[driver]
 
+            print(f'Possible Night Drivers: {night_drivers}')
+
+
+
             min_value = min(night_drivers.values())
             night_drivers = [k for k in night_drivers if night_drivers[k] == min_value]
 
-            # pick better drivers
-            night_shift = random.choice(night_drivers)
+             
 
+
+            night_shift = random.choice(night_drivers)
             possible_route_drivers.remove(night_shift)
+
+            day_drivers = {driver: driver_night_shifts[driver] for driver in possible_route_drivers}
+
+            print(f'Possible day drivers: {day_drivers}')
+
+            max_value = max(day_drivers.values())
+            day_drivers = [k for k in day_drivers if day_drivers[k] == max_value]
+
             day_shift = random.choice(possible_route_drivers)
 
             route_result[route_name] = {
@@ -96,6 +125,7 @@ def process() -> typing.DefaultDict[str, typing.Any]:
 
         result[day] = route_result
     return result
+
 
 def write_to_csv():
     data = process()
@@ -111,5 +141,6 @@ def write_to_csv():
         writer.writerow(['Driver ID', 'Day', 'Route ID', 'Shift ID'])
 
         writer.writerows(cool_list)
+
 
 write_to_csv()
